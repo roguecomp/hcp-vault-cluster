003c9ab6393b4c38f14aa9ec4fa9570d4b5cc25a
vish kap <roguecomp001@gmail.com>
2024-06-16 08:02:27 +0000

    removed unused code

--- main.tf
+++ main.tf
@@ -44,63 +44,6 @@ data "hcp_vault_secrets_secret" "secret_access_key" {
   secret_name = "SECRET_ACCESS_KEY"
 }
 
-provider "vault" {
-  token     = hcp_vault_cluster_admin_token.vault.token
-  address   = hcp_vault_cluster.vault.vault_public_endpoint_url
-  namespace = "admin"
-}
-
-resource "hcp_vault_cluster_admin_token" "vault" {
-  cluster_id = "vault-cluster"
-
-  depends_on = [hcp_vault_cluster.vault]
-}
-
-resource "vault_policy" "up-bank" {
-  name   = "up-bank"
-  policy = file("policies/up-bank.hcl")
-}
-
-resource "vault_mount" "kv" {
-  path = "kv"
-  type = "kv-v2"
-}
-
-resource "vault_aws_secret_backend" "aws" {
-  access_key = data.hcp_vault_secrets_secret.access_key.secret_value
-  secret_key = data.hcp_vault_secrets_secret.secret_access_key.secret_value
-  path       = "aws"
-}
-
-#resource "vault_aws_secret_backend_role" "role" {
-#  backend         = vault_aws_secret_backend.aws.path
-#  name            = "deploy"
-#  credential_type = "iam_user"
-#
-#  policy_document = <<EOT
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Effect": "Allow",
-#      "Action": "iam:*",
-#      "Resource": "*"
-#    }
-#  ]
-#}
-#EOT
-#}
-
-# data "hcp_vault_secrets_secret" "cloudflare_token" {
-#   app_name    = "cloudflare"
-#   secret_name = "CLOUDFLARE_API_TOKEN"
-# }
-
-# data "hcp_vault_secrets_secret" "cloudflare_zone" {
-#   app_name    = "cloudflare"
-#   secret_name = "ZONE_ID"
-# }
-
 # provider "cloudflare" {
 #   api_token = data.hcp_vault_secrets_secret.cloudflare_token.secret_value
 # }
